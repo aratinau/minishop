131a0a94a817382689eb80b4b85252123de29df0
Aymeric <hello@codeparadise.xyz>
2020-10-14 15:46:29 +0200

    img src removed

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require('header.php');
 	<div class="row">
       <div class="col-lg-4">
         <h2>Heading</h2>
-		<img src="https://loremflickr.com/g/350/260/product" />
+		<img src="" />
         <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
         <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
       </div>
