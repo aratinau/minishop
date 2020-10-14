e2edf8dd2b94e58016895468e06ecdbfa6535058
Aymeric <hello@codeparadise.xyz>
2020-10-14 15:45:45 +0200

    display product and data products

--- database/products.sql
+++ database/products.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le :  mer. 14 oct. 2020 à 10:39
+-- Généré le :  mer. 14 oct. 2020 à 15:44
 -- Version du serveur :  5.7.26
 -- Version de PHP :  5.6.40
 
@@ -24,15 +24,24 @@ CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `name` text NOT NULL,
   `description` text NOT NULL,
-  `price` int(11) NOT NULL
+  `price` int(11) NOT NULL,
+  `img` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `description`, `price`) VALUES
-(1, 'jambon', 'tres bonne qualité', 10);
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `img`) VALUES
+(1, 'jambon', 'tres bonne qualité', 10, 'https://loremflickr.com/g/350/260/product'),
+(2, 'bonbon', 'tres sucre', 20, 'https://loremflickr.com/g/350/260/product'),
+(3, 'pain', 'bonne farine', 34, 'https://loremflickr.com/g/350/260/product'),
+(4, 'cornichon', 'tres croquant', 12, 'https://loremflickr.com/g/350/260/product'),
+(5, 'lait', 'de vache', 45, 'https://loremflickr.com/g/350/260/product'),
+(6, 'fromage', 'pour sandwitch', 46, 'https://loremflickr.com/g/350/260/product'),
+(7, 'beurre', 'breton', 9, 'https://loremflickr.com/g/350/260/product'),
+(8, 'mayo', 'de dijon', 14, 'https://loremflickr.com/g/350/260/product'),
+(9, 'moutarde', 'de dijon', 11, 'https://loremflickr.com/g/350/260/product');
 
 --
 -- Index pour les tables déchargées
@@ -52,5 +61,5 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT pour la table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
--- index.php
+++ index.php
@@ -6,11 +6,12 @@ require('init.php');
 require('header.php');
 ?>
 	<h1>Mini Shop</h1>
-	<?php 
-		foreach($dbh->query('SELECT * from products') as $row) {
-			echo '<pre>';
-			print_r($row);
-			echo '<pre>';
-		}
-	?>
+	<div class="row">
+      <div class="col-lg-4">
+        <h2>Heading</h2>
+		<img src="https://loremflickr.com/g/350/260/product" />
+        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+      </div>
+    </div>
 <?php require('footer.php'); ?>
