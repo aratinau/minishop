ef1c3d07dc14c6f8e6ca814121400010a609031a
Aymeric <hello@codeparadise.xyz>
2020-10-14 11:35:53 +0200

    explode files

--- index.php
+++ index.php
@@ -1,39 +1,14 @@
 <?php
 require('config.php');
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname='.DBNAME, USER, PASS);
-    foreach($dbh->query('SELECT * from products') as $row) {
-        echo '<pre>';
-        print_r($row);
-        echo '<pre>';
-    }
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
-}
+require('init.php');
 ?>
-<!doctype html>
-<html lang="en" class="">
-	<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-	<meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-	<meta name="generator" content="Jekyll v4.1.1">
-	<title>Mini Shop</title>
-	<!-- CSS only -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
-	<!-- JS, Popper.js, and jQuery -->
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-	</head>
-	<body class="">
-        <div class="container">
-              <h1>Mini Shop</h1>
-        </div>
-	</body>
-</html>
-
+<?php require('header.php'); ?>
+	<h1>Mini Shop</h1>
+	<?php 
+		foreach($dbh->query('SELECT * from products') as $row) {
+			echo '<pre>';
+			print_r($row);
+			echo '<pre>';
+		}
+	?>
+<?php require('footer.php'); ?>
